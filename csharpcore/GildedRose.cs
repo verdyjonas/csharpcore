b2e91fef4b155efe92e04610ee97563e2a9704ab
verdyjonas <47108742+verdyjonas@users.noreply.github.com>
2021-01-19 15:53:55 +0100

    Add files via upload

---
+++ csharpcore/GildedRose.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections.Generic;
+
+namespace csharpcore
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+
+        public void MyUpDateQuality()
+        {
+            int Downday = 1;
+            int Max = 50;
+            int Min = 0;
+            int result = 0;           
+          
+            foreach (var i in Items)
+            {
+              
+                   i.SellIn -= Downday;
+                  switch (i.Name)
+                  {
+                    case ("Sulfuras, Hand of Ragnaros"):
+                        {                        
+                                break; 
+                        }
+
+                    case ("Backstage passes to a TAFKAL80ETC concert"):
+                        {
+                            if (i.SellIn <= 10 && i.SellIn >= 5) { i.Quality = 2; }
+                            else if (i.SellIn >= 5) { i.Quality = 3; }
+                            break;
+                        }
+                    case ("Aged Brie"):
+                        {
+                           if (i.SellIn <= 10 && i.SellIn <= 5)
+                            {
+                                result = i.Quality + 2;
+                               if (result > Max) { i.Quality = Max; }
+                               else { i.Quality = result; }
+                           }
+                                break;
+                        }
+                    case ("Conjured Mana Cake"):
+                        {
+                              result =  i.Quality - (2 * Downday); 
+                              if(result < 0) { i.Quality = Min; }
+                              else { i.Quality = result; }
+                              break;
+                        }
+                     case ("Elixir of the Mongoose"):
+                            {
+                                if (i.SellIn <= 0)
+                                {
+                                    result = i.Quality - (2 * Downday);
+                                    if (result < 0) { i.Quality = Min; }
+                                    else { i.Quality = result; }
+                                }
+                                else { i.Quality -= Downday; }
+                                break;
+                            }
+                      case ("+5 Dexterity Vest"):
+                            {
+                                if (i.SellIn <= 0)
+                                {
+                                    result = i.Quality - (2 * Downday);
+                                    if (result < 0) { i.Quality = Min; }
+                                    else { i.Quality = result; }
+                                }
+                                else { i.Quality -= Downday; }
+                                break;
+                            }
+                  }
+
+               
+              
+            }
+           
+        }
+        public void UpdateQuality()
+        {
+
+            MyUpDateQuality(); 
+            //for (var i = 0; i < Items.Count; i++)
+            //{
+            //    if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            //    {
+            //        if (Items[i].Quality > 0)
+            //        {
+            //            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+            //            {
+            //                Items[i].Quality = Items[i].Quality - 1;
+            //            }
+            //        }
+            //    }
+            //    else
+            //    {
+            //        if (Items[i].Quality < 50)
+            //        {
+            //            Items[i].Quality = Items[i].Quality + 1;
+
+            //            if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+            //            {
+            //                if (Items[i].SellIn < 11)
+            //                {
+            //                    if (Items[i].Quality < 50)
+            //                    {
+            //                        Items[i].Quality = Items[i].Quality + 1;
+            //                    }
+            //                }
+
+            //                if (Items[i].SellIn < 6)
+            //                {
+            //                    if (Items[i].Quality < 50)
+            //                    {
+            //                        Items[i].Quality = Items[i].Quality + 1;
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+
+            //    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+            //    {
+            //        Items[i].SellIn = Items[i].SellIn - 1;
+            //    }
+
+            //    if (Items[i].SellIn < 0)
+            //    {
+            //        if (Items[i].Name != "Aged Brie")
+            //        {
+            //            if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            //            {
+            //                if (Items[i].Quality > 0)
+            //                {
+            //                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+            //                    {
+            //                        Items[i].Quality = Items[i].Quality - 1;
+            //                    }
+            //                }
+            //            }
+            //            else
+            //            {
+            //                Items[i].Quality = Items[i].Quality - Items[i].Quality; // !!!!     = 0 
+            //            }
+            //        }
+            //        else
+            //        {
+            //            if (Items[i].Quality < 50)
+            //            {
+            //                Items[i].Quality = Items[i].Quality + 1;
+            //            }
+            //        }
+            //    }
+            //}
+        }
+    }
+}
---
+++ csharpcore/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharpcore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("OMGHAI!");
+
+            // SellIn : nombre de jour pour vendre l'article 
+            // une fois la SeelIn négative dégradation deux fois plus vite 
+            // qualité jamais négative et jamais supérieur à 50 sauf sulfuras
+            // Sulfuras jamais être vendu ou qualité diminue
+            // brie agée augmente avec l'age 
+            // backstage pass qualité augmente de 2 quand maximum SellIn 10 et de 3 quand max sellin 5 mais qualité 0 après concert 
+            // conjured diminue deux  fois plus vite que les objets normaux 
+
+            IList<Item> Items = new List<Item>{
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
+				// this conjured item does not work properly yet
+				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+
+            var app = new GildedRose(Items);
+
+
+            for (var i = 0; i < 31; i++)
+            {
+                Console.WriteLine("-------- day " + i + " --------");
+                Console.WriteLine("name, sellIn, quality");
+                for (var j = 0; j < Items.Count; j++)
+                {
+                    System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
+                }
+                Console.WriteLine("");
+                app.UpdateQuality();
+            }
+        }
+    }
+}
